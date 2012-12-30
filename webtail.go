06ccd27c1c4046ffd1958792dd49888db4b6554f
Daisuke Yamashita <dddaisuke@gmail.com>
2012-12-30 23:02:53 +0900

    First commit

---
+++ webtail.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "code.google.com/p/go.net/websocket"
+  "flag"
+  "log"
+  "net/http"
+  "text/template"
+  "bufio"
+  "os"
+)
+
+var line string
+var addr = flag.String("addr", ":8080", "http service address")
+var homeTempl = template.Must(template.ParseFiles("template/index.html"))
+
+func homeHandler(c http.ResponseWriter, req *http.Request) {
+  homeTempl.Execute(c, req.Host)
+}
+
+func main() {
+  flag.Parse()
+  go h.run()
+
+  go func() {
+    reader := bufio.NewReader(os.Stdin);
+
+    for {
+      line,_ := reader.ReadString('\n');
+      h.broadcast <- line
+    }
+  }()
+
+  http.HandleFunc("/", homeHandler)
+  http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+  http.Handle("/ws", websocket.Handler(wsHandler))
+  log.Println("http://localhost:8080/ listen start...")
+  if err := http.ListenAndServe(*addr, nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
+}
