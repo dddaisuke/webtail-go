fed20c69b32616fefc4c0b04ecbea991019dda17
Daisuke Yamashita <dddaisuke@gmail.com>
2012-12-31 11:26:36 +0900

    Fix code formatted by gofmt

--- webtail.go
+++ webtail.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "code.google.com/p/go.net/websocket"
-  "flag"
-  "log"
-  "net/http"
-  "text/template"
-  "bufio"
-  "os"
-  "os/exec"
+	"bufio"
+	"code.google.com/p/go.net/websocket"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"text/template"
 )
 
 var line string
@@ -16,34 +16,34 @@ var addr = flag.String("addr", ":8080", "http service address")
 var homeTempl = template.Must(template.ParseFiles("template/index.html"))
 
 func homeHandler(c http.ResponseWriter, req *http.Request) {
-  homeTempl.Execute(c, req.Host)
+	homeTempl.Execute(c, req.Host)
 }
 
 func main() {
-  flag.Parse()
-  go h.run()
+	flag.Parse()
+	go h.run()
 
-  go func() {
-    reader := bufio.NewReader(os.Stdin);
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
 
-    for {
-      line,_ := reader.ReadString('\n');
-      h.broadcast <- line
-    }
-  }()
+		for {
+			line, _ := reader.ReadString('\n')
+			h.broadcast <- line
+		}
+	}()
 
-  http.HandleFunc("/", homeHandler)
-  http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
-  http.Handle("/ws", websocket.Handler(wsHandler))
+	http.HandleFunc("/", homeHandler)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/ws", websocket.Handler(wsHandler))
 
-  go func() {
-    log.Println("test")
-    cmd := exec.Command("open", "http://localhost:8080/")
-    cmd.Run()
-  }()
+	go func() {
+		log.Println("test")
+		cmd := exec.Command("open", "http://localhost:8080/")
+		cmd.Run()
+	}()
 
-  log.Println("http://localhost:8080/ listen start...")
-  if err := http.ListenAndServe(*addr, nil); err != nil {
-    log.Fatal("ListenAndServe:", err)
-  }
+	log.Println("http://localhost:8080/ listen start...")
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
